6b699f6ef640d265813fef630fbf1562dfe8a9de
Guillermo Fuentes <45331658+blackl1ght98@users.noreply.github.com>
2026-04-28 19:57:31 +0200

    Cambio de firebase a github pages

--- src/app/Proyectos/glucontrol/glucontrol.ts
+++ src/app/Proyectos/glucontrol/glucontrol.ts
@@ -1,23 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import { FirebaseService} from '../../services/firebase.service';
 import { CommonModule } from '@angular/common';
+import { AssetsService } from '../../services/assets.service';
 
 @Component({
   selector: 'app-glucontrol',
   imports: [CommonModule],
   templateUrl: './glucontrol.html',
-  styleUrl: './glucontrol.css'
+  styleUrl: './glucontrol.css',
 })
 export class Glucontrol implements OnInit {
-imageUrls: { [key: string]: string[] } = {};
+  imageUrls: { [key: string]: string[] } = {};
 
-  constructor(private firebaseService: FirebaseService) {}
+  constructor(private assetsService: AssetsService) {}
 
   ngOnInit(): void {
-    this.firebaseService.getImageUrls().then(urls => {
-      this.imageUrls = urls;
-    }).catch(error => {
-      console.error('Error loading image URLs:', error);
-    });
+    this.assetsService
+      .getImageUrls()
+      .then((urls) => {
+        this.imageUrls = urls;
+      })
+      .catch((error) => {
+        console.error('Error loading image URLs:', error);
+      });
   }
 }
---
+++ src/app/services/assets.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AssetsService {
+  getImageUrls(): Promise<{ [key: string]: string[] }> {
+    return Promise.resolve({
+      gestorinventario: [
+        'assets/gestorInventario/carrito.png',
+        'assets/gestorInventario/crear-usuario.png',
+        'assets/gestorInventario/gestion-roles.png',
+        'assets/gestorInventario/login.png',
+        'assets/gestorInventario/panel-admin.png',
+        'assets/gestorInventario/pedidos.png',
+        'assets/gestorInventario/planes-paypal.png',
+        'assets/gestorInventario/productos.png',
+        'assets/gestorInventario/proveedores.png',
+        'assets/gestorInventario/recuperar-contraseña.png',
+        'assets/gestorInventario/rembolsos.png',
+        'assets/gestorInventario/roles.png',
+        'assets/gestorInventario/subscriptores.png',
+      ],
+      glucontrol: ['assets/glucontrol/2.png', 'assets/glucontrol/4.png'],
+    });
+  }
+}
